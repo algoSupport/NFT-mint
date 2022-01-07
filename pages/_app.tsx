c90cf8b1fb4ec9afff8a956cefc6b1a170dd1b8e
algoSupport <algoplusnine@outlook.com>
2022-01-07 13:42:12 +0900

    Use `globals.css` instead of `tailwind.css`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
-// import '../styles/globals.css'
-import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Web3ReactProvider } from '@web3-react/core';
 import { ethers } from 'ethers';
